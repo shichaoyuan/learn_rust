881ba74e86deea926e1bd491f33bb1681e7ed8af
shichaoyuan <oahcihsnauy@gmail.com>
2022-10-10 19:11:57 +0800

    leetcode

---
+++ leetcode/src/longest_substring_without_repeating_characters.rs
@@ -0,0 +1,48 @@
+
+
+struct Solution;
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        use std::collections::HashMap;
+        let mut map: HashMap<u8, i32> = HashMap::new();
+        let mut l = 0;
+        let mut r = 0;
+        let mut longest = 0;
+
+        
+        let bytes = s.as_bytes();
+        loop {
+            if r >= bytes.len() {
+                break;
+            }
+            let a = bytes[r];
+            map.entry(a).and_modify(|c| *c += 1).or_insert(1);
+            r += 1;
+
+            while map.get(&a).map_or(false, |c| *c > 1) {
+                let b = bytes[l];
+                map.entry(b).and_modify(|c| *c -= 1);
+                l += 1;
+            }
+            
+            if r - l > longest {
+                longest = r - l;
+            }
+        }
+
+        longest as i32
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+    
+    #[test]
+    fn test() {
+        let r = Solution::length_of_longest_substring("abcabcbb".to_string());
+        assert_eq!(3, r);
+    }
+
+}
--- leetcode/src/two_sum.rs
+++ leetcode/src/two_sum.rs
@@ -1,4 +1,3 @@
-
 struct Solution;
 
 impl Solution {
@@ -7,10 +6,10 @@ impl Solution {
 
         let mut map = HashMap::new();
         for (i, num) in nums.iter().enumerate() {
-            if let Some(&v) = map.get(&(target-num)) {
+            if let Some(&v) = map.get(&(target - num)) {
                 return vec![i as i32, v];
             }
-            map.insert(num.clone(), i as i32);
+            map.insert(*num, i as i32);
         }
         panic!();
     }
@@ -19,7 +18,7 @@ impl Solution {
 #[cfg(test)]
 mod test {
     use super::Solution;
-    
+
     #[test]
     fn test() {
         let nums = vec![2, 7, 11, 15];
@@ -27,5 +26,4 @@ mod test {
         let r = Solution::two_sum(nums, target);
         assert_eq!(vec![1, 0], r);
     }
-    
 }
